e26086ad8b0b9930e46e49c531cb03ffd96db33e
Anna Selivanova <foxy.eyed@gmail.com>
2022-10-30 18:51:34 +0200

    Respond with jwt token

--- app/routes/session_routes.rb
+++ app/routes/session_routes.rb
@@ -4,7 +4,11 @@ class SessionRoutes < Application
   post "/" do
     result = Sessions::Create.new.call(params)
     if result.success?
+      session = result.value!
+      meta = { token: JwtEncoder.new(Settings.app.secret).encode(uuid: session.uuid) }
+
       status 201
+      json meta: meta
     else
       process_failure(result)
     end
