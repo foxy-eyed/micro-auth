90b943905afd763c0b101ca0c4bcf8b54fa7ac0b
Anna Selivanova <foxy.eyed@gmail.com>
2022-10-31 10:09:33 +0200

    Fix db connection on CI

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+Config.setup do |config|
+  config.use_env = true
+  config.env_prefix = "ENV"
+  config.env_separator = "__"
+end
+
 setting_files = Config.setting_files(
   File.expand_path("..", __dir__),
   ENV.fetch("RACK_ENV")
